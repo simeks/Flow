169fde36f7935ba9a9ff5cfb54abf52b897241b1
Simon <simon.ekstrom@gmail.com>
2016-08-06 06:59:09 +0200

    Changes to CudaImage: User can now specify a flag, indicating that the image is to be used as a texture. If the flag is set the image is allocated as a cudaArray, otherwise we only allocate a linear memory block for the image.

--- Source/Core/Cuda/CudaImage.cu
+++ Source/Core/Cuda/CudaImage.cu
@@ -4,58 +4,116 @@
 
 #include "Image/Image.h"
 
-CudaImage::CudaData::CudaData(int ndims, const Vec3i& size, size_t elem_size)
+namespace
 {
-    if (ndims == 1)
+    cudaChannelFormatDesc create_channel_desc(int pixel_type)
     {
-        CUDA_CHECK_ERRORS(cudaMalloc(&data, elem_size * size.x));
-        pitch = elem_size * size.x;
+        switch (pixel_type)
+        {
+        case image::PixelType_UInt8:
+        case image::PixelType_UInt16:
+        case image::PixelType_UInt32:
+        {
+            int e = (int)image::pixel_size(pixel_type) * 8;
+            return cudaCreateChannelDesc(e, 0, 0, 0, cudaChannelFormatKindUnsigned);
+        }
+        case image::PixelType_Float32:
+        case image::PixelType_Float64:
+        {
+            int e = (int)image::pixel_size(pixel_type) * 8;
+            return cudaCreateChannelDesc(e, 0, 0, 0, cudaChannelFormatKindFloat);
+        }
+        case image::PixelType_Vec3f:
+        {
+            int e = (int)sizeof(float) * 8;
+            return cudaCreateChannelDesc(e, e, e, 0, cudaChannelFormatKindFloat);
+        }
+        case image::PixelType_Vec3d:
+        {
+            int e = (int)sizeof(double) * 8;
+            return cudaCreateChannelDesc(e, e, e, 0, cudaChannelFormatKindFloat);
+        }
+        case image::PixelType_Vec4u8:
+        {
+            int e = (int)sizeof(uint8_t) * 8;
+            return cudaCreateChannelDesc(e, e, e, e, cudaChannelFormatKindUnsigned);
+        }
+        case image::PixelType_Vec4f:
+        {
+            int e = (int)sizeof(float) * 8;
+            return cudaCreateChannelDesc(e, e, e, e, cudaChannelFormatKindFloat);
+        }
+        }
+        return cudaCreateChannelDesc(0, 0, 0, 0, cudaChannelFormatKindNone);
     }
-    else if (ndims == 2)
+}
+
+CudaImage::CudaData::CudaData(int ndims, const Vec3i& size, int type, int flags) :
+    data(nullptr),
+    arr(nullptr)
+{
+    if (flags & Flag_Texture)
     {
-        CUDA_CHECK_ERRORS(cudaMallocPitch(&data, &pitch, elem_size * size.x, size.y));
+        // There are certain memory requirements (padding, etc) for images that are to be bound as textures
+
+        cudaChannelFormatDesc desc = create_channel_desc(type);
+        if (ndims == 3)
+        {
+            CUDA_CHECK_ERRORS(cudaMalloc3DArray(&arr, &desc, make_cudaExtent(size.x, size.y, size.z)));
+        }
+        else
+        {
+            CUDA_CHECK_ERRORS(cudaMallocArray(&arr, &desc, size.x, size.y));
+        }
     }
     else
     {
-        cudaPitchedPtr ptr;
-        cudaExtent extent = make_cudaExtent(elem_size * size.x, size.y, size.z);
-        CUDA_CHECK_ERRORS(cudaMalloc3D(&ptr, extent));
-
-        data = (uint8_t*)ptr.ptr;
-        pitch = ptr.pitch;
+        // Otherwise we just allocate it linearly
+        size_t elem_size = image::pixel_size(type);
+        CUDA_CHECK_ERRORS(cudaMalloc(&data, elem_size * size.x * size.y * size.z));
     }
+
 }
 CudaImage::CudaData::~CudaData()
 {
-    cudaFree(data);
+    if (data)
+        cudaFree(data);
+    if (arr)
+        cudaFreeArray(arr);
     data = nullptr;
+    arr = nullptr;
 }
 
 CudaImage::CudaImage() :
-    _data(nullptr),
-    _ndims(0),
-    _pixel_type(image::PixelType_Unknown)
+_data(nullptr),
+_ndims(0),
+_pixel_type(image::PixelType_Unknown),
+_flags(0)
 {
 }
-CudaImage::CudaImage(const Image& img)
+CudaImage::CudaImage(const Image& img, int flags)
 {
-    upload(img);
+    upload(img, flags);
 }
-CudaImage::CudaImage(int ndims, const Vec3i& size, int type)
+CudaImage::CudaImage(int ndims, const Vec3i& size, int type, int flags)
 {
-    create(ndims, size, type);
+    create(ndims, size, type, flags);
 }
 CudaImage::~CudaImage()
 {
     _data.reset();
 }
 
-void CudaImage::create(int ndims, const Vec3i& size, int type)
+void CudaImage::create(int ndims, const Vec3i& size, int type, int flags)
 {
     assert(ndims <= 3);
     assert(type != image::PixelType_Unknown);
 
-    if (_ndims == ndims && _size == size && _pixel_type == type && _data)
+    if (_ndims == ndims && 
+        _size == size && 
+        _pixel_type == type && 
+        _flags == flags && 
+        _data)
         return;
 
     release();
@@ -63,37 +121,41 @@ void CudaImage::create(int ndims, const Vec3i& size, int type)
     _ndims = ndims;
     _size = size;
     _pixel_type = type;
+    _flags = flags;
 
-    size_t elem_size = image::pixel_size(type);
-    _data = std::make_shared<CudaData>(ndims, size, elem_size);
+    _data = std::make_shared<CudaData>(ndims, size, type, flags);
 }
 void CudaImage::release()
 {
     _pixel_type = image::PixelType_Unknown;
     _data.reset();
 }
-void CudaImage::upload(const Image& img)
+void CudaImage::upload(const Image& img, int flags)
 {
-    create(img.ndims(), img.size(), img.pixel_type());
+    create(img.ndims(), img.size(), img.pixel_type(), flags);
 
     size_t elem_size = image::pixel_size(_pixel_type);
-    if (_ndims == 1)
+    if (flags & Flag_Texture)
     {
-        CUDA_CHECK_ERRORS(cudaMemcpy(_data->data, img.ptr(), elem_size * _size.x, cudaMemcpyHostToDevice));
-    }
-    else if (_ndims == 2)
-    {
-        CUDA_CHECK_ERRORS(cudaMemcpy2D(_data->data, _data->pitch, img.ptr(), img.step()[1], elem_size * _size.x, _size.y, cudaMemcpyHostToDevice));
+        if (_ndims == 3)
+        {
+            cudaMemcpy3DParms params = { 0 };
+            params.srcPtr = make_cudaPitchedPtr((void*)img.ptr(), img.size().x*elem_size, img.size().x, img.size().y);
+            params.dstArray = _data->arr;
+            params.extent = make_cudaExtent(img.size().x, img.size().y, img.size().z);
+            params.kind = cudaMemcpyHostToDevice;
+            CUDA_CHECK_ERRORS(cudaMemcpy3D(&params));
+        }
+        else
+        {
+            CUDA_CHECK_ERRORS(cudaMemcpyToArray(_data->arr, 0, 0, img.ptr(),
+                elem_size * _size.x * _size.y * _size.z, cudaMemcpyHostToDevice));
+        }
     }
     else
     {
-        cudaMemcpy3DParms parms = { 0 };
-        parms.dstPtr = make_cudaPitchedPtr(_data->data, _data->pitch, elem_size * _size.x, _size.y);
-        parms.srcPtr = make_cudaPitchedPtr((void*)img.ptr(), img.step()[1], elem_size * img.size().x, img.size().y);
-        parms.extent = make_cudaExtent(elem_size * _size.x, _size.y, _size.z);
-        parms.kind = cudaMemcpyHostToDevice;
-        
-        CUDA_CHECK_ERRORS(cudaMemcpy3D(&parms));
+        CUDA_CHECK_ERRORS(cudaMemcpy(_data->data, img.ptr(), 
+            elem_size * _size.x * _size.y * _size.z, cudaMemcpyHostToDevice));
     }
 }
 void CudaImage::download(Image& img)
@@ -101,30 +163,53 @@ void CudaImage::download(Image& img)
     img.create(_ndims, _size, _pixel_type);
 
     size_t elem_size = image::pixel_size(_pixel_type);
-    if (_ndims == 1)
+    if (_flags & Flag_Texture)
     {
-        CUDA_CHECK_ERRORS(cudaMemcpy(img.ptr(), _data->data, elem_size * _size.x, cudaMemcpyDeviceToHost));
-    }
-    else if (_ndims == 2)
-    {
-        CUDA_CHECK_ERRORS(cudaMemcpy2D(img.ptr(), img.step()[1], _data->data, _data->pitch, elem_size * _size.x, _size.y, cudaMemcpyDeviceToHost));
+        if (_ndims == 3)
+        {
+            cudaMemcpy3DParms params = { 0 };
+            params.dstPtr = make_cudaPitchedPtr((void*)img.ptr(), img.size().x*elem_size, img.size().x, img.size().y);
+            params.srcArray = _data->arr;
+            params.extent = make_cudaExtent(img.size().x, img.size().y, img.size().z);
+            params.kind = cudaMemcpyDeviceToHost;
+            CUDA_CHECK_ERRORS(cudaMemcpy3D(&params));
+        }
+        else
+        {
+            CUDA_CHECK_ERRORS(cudaMemcpyFromArray(img.ptr(), _data->arr, 0, 0,
+                elem_size * _size.x * _size.y * _size.z, cudaMemcpyDeviceToHost));
+        }
     }
     else
     {
-        cudaMemcpy3DParms parms = { 0 };
-        parms.dstPtr = make_cudaPitchedPtr((void*)img.ptr(), img.step()[1], elem_size * img.size().x, img.size().y);
-        parms.srcPtr = make_cudaPitchedPtr(_data->data, _data->pitch, elem_size * _size.x, _size.y);
-        parms.extent = make_cudaExtent(elem_size * _size.x, _size.y, _size.z);
-        parms.kind = cudaMemcpyDeviceToHost;
-
-        CUDA_CHECK_ERRORS(cudaMemcpy3D(&parms));
+        CUDA_CHECK_ERRORS(cudaMemcpy(img.ptr(), _data->data, 
+            elem_size * _size.x * _size.y * _size.z, cudaMemcpyDeviceToHost));
     }
 }
+int CudaImage::flags() const
+{
+    return _flags;
+}
+uint8_t* CudaImage::ptr()
+{
+    assert(_data);
+    return _data->data;
+}
+cudaArray_t CudaImage::cuda_array()
+{
+    assert(_data);
+    return _data->arr;
+}
+cudaExtent CudaImage::cuda_extent()
+{
+    return make_cudaExtent(_size.x, _size.y, _size.z);
+}
 CudaImage::CudaImage(const CudaImage& other) :
-    _data(other._data),
-    _ndims(other._ndims),
-    _size(other._size),
-    _pixel_type(other._pixel_type)
+_data(other._data),
+_ndims(other._ndims),
+_size(other._size),
+_pixel_type(other._pixel_type),
+_flags(other._flags)
 {
 }
 CudaImage& CudaImage::operator=(const CudaImage& other)
@@ -133,6 +218,7 @@ CudaImage& CudaImage::operator=(const CudaImage& other)
     _ndims = other._ndims;
     _size = other._size;
     _pixel_type = other._pixel_type;
+    _flags = other._flags;
 
     return *this;
 }
--- Source/Core/Cuda/CudaImage.h
+++ Source/Core/Cuda/CudaImage.h
@@ -1,40 +1,55 @@
 #ifndef __CUDA_IMAGE_H__
 #define __CUDA_IMAGE_H__
 
-#include "Image/Vec3.h"
+#include <Core/Image/Vec3.h>
 
 class Image;
 class CORE_API CudaImage
 {
 public:
+    enum Flags
+    {
+        Flag_Texture = 1 //< Indicates that this object is to be used as a read-only texture.
+    };
+
     CudaImage();
     /// Allocates memory on the GPU and uploads data of the specified image.
-    CudaImage(const Image& img);
+    CudaImage(const Image& img, int flags = 0);
     /// Allocates memory on the GPU for a image with the specified properties.
-    CudaImage(int ndims, const Vec3i& size, int type);
+    CudaImage(int ndims, const Vec3i& size, int type, int flags = 0);
     ~CudaImage();
 
     /// @remark This does not copy the data, use clone if you want a separate copy.
     CudaImage(const CudaImage& other);
     CudaImage& operator=(const CudaImage& other);
 
-    void create(int ndims, const Vec3i& size, int type);
+    void create(int ndims, const Vec3i& size, int type, int flags = 0);
     void release();
 
     /// Uploads the data from host to device memory.
-    void upload(const Image& img);
+    void upload(const Image& img, int flags = 0);
 
     /// Downloads the data from device to host memory.
     void download(Image& img);
 
+    int flags() const;
+
+    uint8_t* ptr();
+    cudaArray_t cuda_array();
+    cudaExtent cuda_extent();
+
+    template<typename T> 
+    T* ptr() { return (T*)ptr(); }
+    
 private:
     struct CORE_API CudaData
     {
-        CudaData(int ndims, const Vec3i& size, size_t elem_size);
+        CudaData(int ndims, const Vec3i& size, int type, int flags = 0);
         ~CudaData();
 
-        uint8_t* data; 
-        size_t pitch;
+        uint8_t* data;
+        cudaArray_t arr;
+        
     };
     typedef std::shared_ptr<CudaData> DataPtr;
 
@@ -43,6 +58,7 @@ private:
     Vec3i _size;
 
     int _pixel_type;
+    int _flags;
 };
 
 #endif // __CUDA_IMAGE_H__
